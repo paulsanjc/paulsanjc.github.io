3213f0c22029ee9b9f0488e3f6ce9641032b4094
paulsanjc <113803357+paulsanjc@users.noreply.github.com>
2023-03-07 22:44:32 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,13 +10,11 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
